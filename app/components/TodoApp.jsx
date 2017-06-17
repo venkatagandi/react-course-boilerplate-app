ed2378018539be5f7ae01f3aa0bf29f9986f5433
venkatagandi <venkata.gandi@gmail.com>
2017-06-17 00:43:12 -0400

    added static data

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,13 +1,35 @@
 var React = require('react');
-
+var TodoList = require('TodoList');
 var TodoApp = React.createClass({
+    getInitialState() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                } , {
+                    id: 2,
+                    text: 'Clean the yard'
+                } , {
+                    id: 3,
+                    text: 'Leave mail on porch'
+                } , {
+                    id: 4,
+                    text: 'Play vedio games'
+                }
+            ]
+        }
+    },
     render() {
+        var { todos } = this.state;
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     }
 });
 
 module.exports = TodoApp;
+
+// <TodoList todos={todos}/>
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var Todo = require('Todo');
+var TodoList = React.createClass({
+    render() {
+        var { todos } = this.props;
+        var renderTodos = () => {
+            return todos.map((todo)=>{
+                return (
+                    <Todo key={todo.id} {...todo}/>
+                )
+            });
+        };
+        return(
+            <div>{renderTodos()}</div>
+        )
+    }
+});
+
+module.exports = TodoList;
